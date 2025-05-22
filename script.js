686758ead628877bc1a5d00471300a149998e67c
HetviGhelani <hetvighelani@gmail.com>
2025-05-22 23:47:50 +0530

    Expense Tracker

--- script.js
+++ script.js
@@ -1,67 +1,70 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const products = [
-    { id: 1, name: "Product 1", price: 29.99 },
-    { id: 2, name: "Product 2", price: 19.99 },
-    { id: 3, name: "Product 3", price: 59.999 },
-  ];
+  const exForm = document.getElementById("expense-form");
+  const exName = document.getElementById("expense-name");
+  const exAmount = document.getElementById("expense-amount");
+  const exList = document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount");
 
-  const cart = [];
+  let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+  let totalAmount = calculateTotal();
 
-  const productList = document.getElementById("product-list");
-  const cartItems = document.getElementById("cart-items");
-  const emptyCartMessage = document.getElementById("empty-cart");
-  const cartTotalMessage = document.getElementById("cart-total");
-  const totalPriceDisplay = document.getElementById("total-price");
-  const checkOutBtn = document.getElementById("checkout-btn");
+  renderExpenses();
 
-  products.forEach((product) => {
-    const productDiv = document.createElement("div");
-    productDiv.classList.add("product");
-    productDiv.innerHTML = `
-      <span>${product.name} - $${product.price.toFixed(2)}</span>
-      <button data-id="${product.id}">Add to cart</button>
-      `;
-    productList.appendChild(productDiv);
+  exForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = exName.value.trim();
+    const amount = parseFloat(exAmount.value.trim());
+
+    if (name !== "" && !isNaN(amount) && amount > 0) {
+      const newExpense = {
+        id: Date.now(),
+        name: name,
+        amount: amount,
+      };
+      expenses.push(newExpense);
+      saveExpensesTolocal();
+      renderExpenses();
+      updateTotal();
+
+      //clear input
+      exName.value = "";
+      exAmount.value = "";
+    }
   });
 
-  productList.addEventListener("click", (e) => {
+  function renderExpenses() {
+    exList.innerHTML = "";
+    expenses.forEach((expense) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+          ${expense.name} - $${expense.amount}
+          <button data-id="${expense.id}">Delete</button>
+          `;
+      exList.appendChild(li);
+    });
+  }
+
+  function calculateTotal() {
+    return expenses.reduce((sum, expense) => sum + expense.amount, 0);
+  }
+
+  function saveExpensesTolocal() {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
+
+  function updateTotal() {
+    totalAmount = calculateTotal();
+    totalAmountDisplay.textContent = totalAmount.toFixed(2);
+  }
+
+  exList.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
-      const productId = parseInt(e.target.getAttribute("data-id"));
-      const product = products.find((p) => p.id === productId);
-      addToCart(product);
+      const expenseId = parseInt(e.target.getAttribute("data-id"));
+      expenses = expenses.filter((expense) => expense.id !== expenseId);
+
+      saveExpensesTolocal();
+      renderExpenses();
+      updateTotal();
     }
-  });
-
-  function addToCart(product) {
-    cart.push(product);
-    renderCart();
-  }
-
-  function renderCart() {
-    cartItems.innerText = "";
-    let totalPrice = 0;
-
-    if (cart.length > 0) {
-      emptyCartMessage.classList.add("hidden");
-      cartTotalMessage.classList.remove("hidden");
-      cart.forEach((item, index) => {
-        totalPrice += item.price;
-        const cartItem = document.createElement("div");
-        cartItem.innerHTML = `
-            ${item.name} - $${item.price.toFixed(2)}
-            `;
-        cartItems.appendChild(cartItem);
-        totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`;
-      });
-    } else {
-      emptyCartMessage.classList.remove("hidden");
-      totalPriceDisplay.textContent = `$0.00`;
-    }
-  }
-
-  checkOutBtn.addEventListener("click", () => {
-    cart.length = 0;
-    alert("Checkout successfully");
-    renderCart();
   });
 });
